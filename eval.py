e6b3d15a78b657ed78f9d79ee8cefbf300e8e82b
Ayushi <ayushisankhala2609@gmail.com>
2024-02-27 13:23:05 +0530

    deleted redundant files

--- eval.py
+++ eval.py
@@ -180,11 +180,11 @@ def _eval_dataset(model, dataset, width, softmax_temp, opts, device):
             )
         duration = time.time() - start
         for seq, cost in zip(sequences, costs):
-            if model.problem.NAME == "tsp":
-                seq = seq.tolist()  # No need to trim as all are same length
-            elif model.problem.NAME in ("cvrp", "sdvrp"):
-                seq = np.trim_zeros(seq).tolist() + [0]  # Add depot
-            elif model.problem.NAME in ("op", "pctsp"):
+            # if model.problem.NAME == "tsp":
+            #     seq = seq.tolist()  # No need to trim as all are same length
+            # elif model.problem.NAME in ("cvrp", "sdvrp"):
+            #     seq = np.trim_zeros(seq).tolist() + [0]  # Add depot
+            if model.problem.NAME in ("op", "pctsp"):
                 seq = np.trim_zeros(seq)  # We have the convention to exclude the depot
             else:
                 assert False, "Unkown problem: {}".format(model.problem.NAME)
