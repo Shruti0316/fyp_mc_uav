fc233c9ba13c35099e0eca4f4728cb9a32a6adfd
Shruti0316 <shruti16.k@gmail.com>
2024-02-27 15:24:33 +0530

    deleted: redundant files

--- create_dataset.py
+++ create_dataset.py
@@ -8,26 +8,6 @@ from sklearn.cluster import KMeans
 from fcmeans import FCM
 from tqdm import tqdm
 
-
-def generate_tsp_data(dataset_size, tsp_size):
-    return np.random.uniform(size=(dataset_size, tsp_size, 2)).tolist()
-
-
-def generate_vrp_data(dataset_size, vrp_size):
-    CAPACITIES = {
-        10: 20.,
-        20: 30.,
-        50: 40.,
-        100: 50.
-    }
-    return list(zip(
-        np.random.uniform(size=(dataset_size, 2)).tolist(),  # Depot location
-        np.random.uniform(size=(dataset_size, vrp_size, 2)).tolist(),  # Node locations
-        np.random.randint(1, 10, size=(dataset_size, vrp_size)).tolist(),  # Demand, uniform integer 1 ... 9
-        np.full(dataset_size, CAPACITIES[vrp_size]).tolist()  # Capacity, same for whole dataset
-    ))
-
-
 def generate_op_data(dataset_size, op_size, max_length, prize_type='const', num_agents=1, num_depots=1, cluster='kmc'):
     depot = np.random.uniform(size=(dataset_size, 2))
     loc = np.random.uniform(size=(dataset_size, op_size, 2))
@@ -89,46 +69,6 @@ def generate_op_data(dataset_size, op_size, max_length, prize_type='const', num_
     ))
 
 
-def generate_pctsp_data(dataset_size, pctsp_size, penalty_factor=3):
-    depot = np.random.uniform(size=(dataset_size, 2))
-    loc = np.random.uniform(size=(dataset_size, pctsp_size, 2))
-
-    # For the penalty to make sense it should be not too large (in which case all nodes will be visited) nor too small
-    # so we want the objective term to be approximately equal to the length of the tour, which we estimate with half
-    # of the nodes by half of the tour length (which is very rough but similar to op)
-    # This means that the sum of penalties for all nodes will be approximately equal to the tour length (on average)
-    # The expected total (uniform) penalty of half of the nodes (since approx half will be visited by the constraint)
-    # is (n / 2) / 2 = n / 4 so divide by this means multiply by 4 / n,
-    # However instead of 4 we use penalty_factor (3 works well) so we can make them larger or smaller
-    MAX_LENGTHS = {
-        20: 2.,
-        50: 3.,
-        100: 4.
-    }
-    penalty_max = MAX_LENGTHS[pctsp_size] * (penalty_factor) / float(pctsp_size)
-    penalty = np.random.uniform(size=(dataset_size, pctsp_size)) * penalty_max
-
-    # Take uniform prizes
-    # Now expectation is 0.5 so expected total prize is n / 2, we want to force to visit approximately half of the nodes
-    # so the constraint will be that total prize >= (n / 2) / 2 = n / 4
-    # equivalently, we divide all prizes by n / 4 and the total prize should be >= 1
-    deterministic_prize = np.random.uniform(size=(dataset_size, pctsp_size)) * 4 / float(pctsp_size)
-
-    # In the deterministic setting, the stochastic_prize is not used and the deterministic prize is known
-    # In the stochastic setting, the deterministic prize is the expected prize and is known up front but the
-    # stochastic prize is only revealed once the node is visited
-    # Stochastic prize is between (0, 2 * expected_prize) such that E(stochastic prize) = E(deterministic_prize)
-    stochastic_prize = np.random.uniform(size=(dataset_size, pctsp_size)) * deterministic_prize * 2
-
-    return list(zip(
-        depot.tolist(),
-        loc.tolist(),
-        penalty.tolist(),
-        deterministic_prize.tolist(),
-        stochastic_prize.tolist()
-    ))
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--name", type=str, required=True, help="Name to identify dataset (test, validation...)")
@@ -175,6 +115,7 @@ if __name__ == "__main__":
                             str(graph_size)
                         )
                     else:
+                        '''CAN REMOVE IT'''
                         data_dir = os.path.join(
                             opts.data_dir,
                             opts.problem,
@@ -190,13 +131,8 @@ if __name__ == "__main__":
                         dataset = generate_op_data(opts.dataset_size, graph_size, prize_type=distribution,
                                                    max_length=opts.max_length[i], num_depots=opts.num_depots,
                                                    num_agents=num_agents, cluster=cluster)
-                    elif opts.problem == 'tsp':
-                        dataset = generate_tsp_data(opts.dataset_size, graph_size)
-                    elif opts.problem == 'pctsp':
-                        dataset = generate_pctsp_data(opts.dataset_size, graph_size)
                     else:
-                        assert opts.problem == 'vrp', 'Problem not in list: [op, tsp, vrp, pctsp]'
-                        dataset = generate_vrp_data(opts.dataset_size, graph_size)
+                        raise AssertionError('Problem is not supported')
 
                     # Save dataset
                     if graph_size > 100 and opts.dataset_size > 1e6:
